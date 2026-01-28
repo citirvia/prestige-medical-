68289dd128003e82f2f5356c8fae85383aa98885
citirvia <130226500+citirvia@users.noreply.github.com>
2026-01-28 20:42:52 +0300

    Delete src directory

--- src/i18n/settings.ts
+++
@@ -1,2 +0,0 @@
-export const locales = ['en', 'fr', 'ar'];
-export const defaultLocale = 'fr';
--- src/navigation.ts
+++
@@ -1,5 +0,0 @@
-import {createNavigation} from 'next-intl/navigation';
-import {locales} from './i18n/settings';
- 
-export const {Link, redirect, usePathname, useRouter, getPathname} =
-  createNavigation({locales});
